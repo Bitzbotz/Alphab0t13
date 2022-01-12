167f3a964d9f2a944b51f1c82d8b2556feec2530
Lord angga <94653227+Bitzbotz@users.noreply.github.com>
2022-01-12 15:28:47 +0700

    Update allpayment.js

--- shop/allpayment.js
+++ shop/allpayment.js
@@ -4,28 +4,28 @@ return `PAYMENT BY ZEEONE OFC
 *🏧 BANK*
 
 1. BRI 
-	ㅁ 8881 0887 4350 47326
+	ㅁ Tidak tersedia
 2. BCA 
-	ㅁ 3901 0887 4350 47326
+	ㅁ Tidak tersedia
 3. BNI 
-	ㅁ 881 0887 4350 47326
+	ㅁ Tidak tersedia
 4. PERMATA
-	ㅁ 852 8887 4350 47326
+	ㅁ Tidak tetsedia
 	
 *💰 E-MONEY*
 
 1. GOPAY
-	ㅁ 0887 4350 47326
+	ㅁ Tidak tersedia
 2. OVO
-	ㅁ 0887 4350 47326
+	ㅁ Tidak tetsedia
 3. DANA
-	ㅁ 0887 4350 47326
+	ㅁ Tidak tersedia
 	
 Semuanya atas nama RI****DI
 
 *📲 PULSA*
 
-Tidak tersedia
+083191486089
 	
 Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
 `
